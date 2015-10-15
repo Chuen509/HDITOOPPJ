e6285b2ae684620bb92b7a2bd64e1455f751ca7b
Chuen509 <ccw@Chuen.local>
2015-10-15 14:47:31 +0800

    #002
    delete the useless comment

--- HelloWorld.java
+++ HelloWorld.java
@@ -18,35 +18,5 @@ class HelloWorld {
           System.out.println("My name is");
           System.out.println("Seattle Slew.");
 
-          String name;
-          name = new String("Espresso");
-          System.out.println("Display by normal"); // normal
-          System.out.println(name);
-
-          System.out.println("Display by using method substring");
-          System.out.println(name.substring(2,7));
-
-          System.out.println("Display the length of the string by using methoh length()");
-          System.out.println(name.length( ));
-
-          int a, b, c = 1;
-          a = b = 3;//c = 3;
-          System.out.println("---------");
-          System.out.println(a + " - " + b + " - " + c+ " - ");
-
-          String text;
-          text = new String();
-          text = "I Love Java and Java loves me.";
-          System.out.println("------------");
-          System.out.println("Locate the index position of a substring by useing method indexOf");
-          System.out.println("The substring is \" " + text + " \"-----");
-          System.out.println("the position of \"J\" is " + text.indexOf("J"));
-          System.out.println("the position of \"love\" is " + text.indexOf("love"));
-
-          text = "test" + 1 + 2;
-          System.out.println(text);
-          text = 1 + 2 + "test";
-          System.out.println(text);
-
      } 
 }
